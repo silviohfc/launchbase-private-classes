1ae57dfcd03ba497e9c23e0fae8ca493d07c10f8
Silvio Henrique <silviohfc@gmail.com>
2020-09-16 22:31:07 -0300

    Created and configured main project files

--- routes.js
+++ routes.js
@@ -2,7 +2,15 @@ const express = require('express')
 const routes = express.Router()
 
 routes.get('/', (req, res) => {
-    return res.send("Ok")
+    return res.redirect("/teachers")
+})
+
+routes.get('/teachers', (req, res) => {
+    return res.render("teachers/index")
+})
+
+routes.get('/students', (req, res) => {
+    return res.send("Students")
 })
 
 module.exports = routes
