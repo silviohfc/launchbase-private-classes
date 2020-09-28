28807c69d27c89e08d9dd9d49f2418b2e55ad2b3
Silvio Henrique <silviohfc@gmail.com>
2020-09-28 16:50:46 -0300

    Created local data storage file

--- teachers.js
+++ teachers.js
@@ -1,3 +1,6 @@
+const fs = require('fs')
+const data = require('./data.json')
+
 // Create
 exports.post = (req, res) => {
 
@@ -9,7 +12,17 @@ exports.post = (req, res) => {
         }
     }
 
-    return res.send(req.body)
+
+    req.body.birth = Date.parse(req.body.birth)
+    req.body.created_at = Date.now()
+
+    data.teachers.push(req.body)
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), err => {
+        if (err) return res.send("Write file error!")
+
+        return res.redirect("/teachers")
+    })
 
 }
 
