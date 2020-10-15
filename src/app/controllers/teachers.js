af4acde5a8a4a1e5f8a98946dedc32829256375b
Silvio Henrique <silviohfc@gmail.com>
2020-10-15 16:05:28 -0300

    Added filter to teachers page

--- src/app/controllers/teachers.js
+++ src/app/controllers/teachers.js
@@ -3,9 +3,20 @@ const Teacher = require('../models/Teacher')
 
 module.exports = {
     index(req, res) {
-        Teacher.all(teachers => {
-            return res.render("teachers/index", {teachers})
-        })
+
+        const { filter } = req.query
+
+        if (filter) {
+            Teacher.findBy(filter, teachers => {
+                return res.render("teachers/index", {teachers, filter})
+            })
+        } else {
+            Teacher.all(teachers => {
+                return res.render("teachers/index", {teachers})
+            })
+        }
+
+        
     },
 
     createRedir(req, res) {
